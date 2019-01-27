88aae508af4efc04a724d2d21c428f130850f180
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-27 22:50:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var customerName = "bob";
 
 
 function upperCaseCustomerName() {
-   customerName = var customerName.upcase;
+   customerName = customerName.upcase;
 }
 
 function setBestCustomer() {
